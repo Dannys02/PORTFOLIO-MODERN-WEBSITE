106b1c797eb633abbb7024c903ef7b14f2015c6a
dannys02 <webdannys@gmail.com>
2025-09-13 16:06:26 +0700

    update

--- main.js
+++ main.js
@@ -1,52 +1,39 @@
-// Lightbox untuk sertifikat
-const lightbox = document.getElementById('lightbox');
-const lightboxImg = document.getElementById('lightbox-img');
-const certImages = document.querySelectorAll('.certificate-item img');
-
-certImages.forEach(img => {
-    img.addEventListener('click', () => {
-        lightbox.classList.add('show');
-        lightboxImg.src = img.src;
-    });
-});
-
-// Simulasi submit form
-const form = document.querySelector('form');
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Terima kasih! Pesan Anda telah dikirim.');
-    form.reset();
-});
-
 // Typing animation name home
-  const textMain = " Dannys Martha Favrillia";
-  let index = 0;
-  let isDeleting = false;
-  const target = document.getElementById("name-typing");
+const textMain = " Dannys Martha Favrillia";
+let index = 0;
+let isDeleting = false;
+const target = document.getElementById("name-typing");
 
-  function ketikLoop() {
-    if (!isDeleting) {
-      target.textContent = textMain.substring(0, index + 1);
-      index++;
-
-      if (index === textMain.length) {
-        setTimeout(() => {
-          isDeleting = true;
-          ketikLoop();
-        }, 3000);
-        return;
-      }
-
-    } else {
-      index--;
-      target.textContent = textMain.substring(0, index);
-
-      if (index === 0) {
-        isDeleting = false;
-      }
+function ketikLoop() {
+  if (!isDeleting) {
+    target.textContent = textMain.substring(0, index + 1);
+    index++;
+    
+    if (index === textMain.length) {
+      setTimeout(() => {
+        isDeleting = true;
+        ketikLoop();
+      }, 3000);
+      return;
     }
-
-    setTimeout(ketikLoop, isDeleting ? 50 : 100);
+    
+  } else {
+    index--;
+    target.textContent = textMain.substring(0, index);
+    
+    if (index === 0) {
+      isDeleting = false;
+    }
   }
+  
+  setTimeout(ketikLoop, isDeleting ? 50 : 100);
+}
 
-  ketikLoop();
+ketikLoop();
+
+/* TOMBOL NAVBAR MOBILE */
+const tombolTakSenonoh = document.querySelector("nav ul");
+
+function toggleMenu() {
+  tombolTakSenonoh.classList.toggle("active");
+}
